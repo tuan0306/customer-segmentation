f5f8c3eaed28b53fc6076998ae3b88747a79f008
tuan0306 <nguyentuan22032006@gmail.com>
2026-01-04 18:08:04 +0700

    class FeatureEngineer con ham save_features

--- src/clustering_library.py
+++ src/clustering_library.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy import stats
+from sklearn.preprocessing import StandardScaler
 
 class DataCleaner:
     def __init__(self,file_path):
@@ -119,3 +121,107 @@ class DataVisualizer:
         plt.tight_layout()
         plt.show()
         
+
+class FeatureEngineer:
+    def __init__(self,file_path):
+        self.file_path=file_path
+        self.df=None
+        self.customer_features=None
+        self.customer_features_transformed=None
+        self.customer_features_scaled=None
+        self.scaler = StandardScaler()
+        
+    def load_data(self):
+        self.df=pd.read_csv(self.file_path,encoding='unicode_escape',parse_date=['InvoiceDate','Date','Month'])
+        return self.df
+    
+    def create_customer_features(self):
+        volumn_value=self.df.groupby('CustomerID').agg({
+            'Quantity':'sum',
+            'TotalPrice':'sum',
+            'InvoiceNo':'nunique',
+            'StockCode':'nunique'
+        })
+        
+        volumn_value['Mean_UnitPrice']=volumn_value['TotalPrice']/volumn_value['Quantity']
+        volumn_value=volumn_value.rename(columns={
+            'Quantity':'Sum_Quantity',
+            'TotalPrice':'Sum_TotalPrice',
+            'InvoiceNo':'Count_Invoice',
+            'StockCode':'Count_Stock'
+        })
+        
+        transaction_behaviour_helper=self.df.groupby(['CustomerID','InvoiceNo']).agg({
+            'Quantity':'sum',
+            'TotalPrice':'sum',
+            'StockCode':'nunique'
+        })
+        
+        transaction_behaviour=transaction_behaviour_helper.groupby('CustomerID').agg({
+            'Quantity':'mean',
+            'TotalPrice':'mean',
+            'StockCode':'mean'
+        })
+        
+        transaction_behaviour=transaction_behaviour.rename(columns={
+            'Quantity':'Mean_QuantitySumPerInvoice',
+            'TotalPrice':'Mean_TotalPriceSumPerInvoice',
+            'StockCode':'Mean_StockCountPerInvoice'
+        })
+        
+        product_prefer_helper=self.df.groupby(['CustomerID','StockCode']).agg({
+            'InvoiceNo':'nunique',
+            'UnitPrice':'mean'
+        })
+        
+        product_prefer=product_prefer_helper.groupby('CustomerID').agg({
+            'InvoiceNo':'mean',
+            'UnitPrice':'mean'
+        })
+        
+        product_prefer=product_prefer.rename(columns={
+            'InvoiceNo':'Mean_InvoiceCountPerStock',
+            'UnitPrice':'Mean_UnitPriceMeanPerStock'
+        })
+        
+        self.create_customer_features=pd.concat([volumn_value,transaction_behaviour,product_prefer],axis=1)
+        return self.customer_features
+    
+    def transform_feature(self):
+        customer_features_indexed=self.customer_features.set_index('CustomerID')
+        feature_values=customer_features_indexed.values+1
+        self.customer_features_transformed=customer_features_indexed.copy()
+        for i,feature in enumerate(self.customer_features):
+            transformed,lambda_param=stats.boxcox(feature_values.iloc[:,i])
+            self.customer_features_transformed.iloc[:,i]=transformed
+        return self.customer_features_transformed
+    
+    def plot_features_histograms(self,transform=False):
+        if (transform):
+            data_to_plot=self.customer_features_transformed
+        else:
+            data_to_plot=self.customer_features
+        
+        color='darkred'
+        n_rows=4
+        n_cols=4
+        fig,axes=plt.subplots(n_rows,n_cols,figsize=(20,16))
+        axes=axes.flatten()
+        
+        for i,cols in enumerate(data_to_plot.columns):
+            sns.histplot(data_to_plot[cols],kde=False,ax=axes[i],bins=30)
+            axes[i].set_title(cols)
+            axes[i].set_xlabel('')
+            axes[i].set_ylabel('Tan suat')
+        
+        plt.tight_layout()
+        plt.show()
+        
+    def scale_feature(self):
+        scaled_data=self.scaler.fit_transform(self.customer_features_transformed)
+        self.customer_features_scaled=pd.DataFrame(
+            scaled_data,
+            index=self.customer_features_transformed.index,
+            columns=self.customer_features_transformed.columns)
+        
+        return self.customer_features_scaled
